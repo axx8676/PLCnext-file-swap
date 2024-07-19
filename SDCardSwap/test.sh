ebf9f4bc9bac5dbf908ba4a5f05b6346277736c6
axx8676 <axel.s.thompson-1@ou.edu>
2024-07-19 15:02:26 -0500

    removed test script

--- SDCardSwap/test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-{
-    current_time=$(date "+%m-%d-%Y_%T")
-    new_file_name=test_$current_time
-    echo "$new_file_name"
-    mkdir -p /opt/plcnext/project_archive
-    cp -a /opt/plcnext/projects /opt/plcnext/project_archive/$new_file_name
-} & disown
