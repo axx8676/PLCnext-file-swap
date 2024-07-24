4218b40806b31d00f1202b6fa89eac685b77d009
axx8676 <axel.s.thompson-1@ou.edu>
2024-07-24 12:16:24 -0500

    include project name in output

--- SDCardSwap/Swap.sh
+++ SDCardSwap/Swap.sh
@@ -1,57 +1,85 @@
 #!/bin/bash
 {
-  export PATH=$PATH:/usr/local/bin:/usr/bin:/bin:/usr/local/sbin:/usr/sbin:/sbin:/opt/plcnext/appshome/bin
+export PATH=$PATH:/usr/local/bin:/usr/bin:/bin:/usr/local/sbin:/usr/sbin:/sbin:/opt/plcnext/appshome/bin
 
-  sd_state="$(/usr/sbin/sdcard_state.sh getStatus)"
-  FLAG_FILE="/opt/plcnext/just_rebooted"
-  current_time=$(date "+%m-%d-%Y_%T")
-  archive_file_name=projects_$current_time
+FLAG_FILE="/opt/plcnext/just_rebooted"
+current_time=$(date "+%m-%d-%Y_%T")
+archive_file_name=projects_$current_time
 
-  function fileTransfer() {
-    sudo /usr/sbin/sdcard_state.sh request_deactivation
-    echo "SD card deactivated"
+PLC_project_name=$(python3 -c "import sys, json;
+f = open('/media/rfs/internalsd/upperdir/opt/plcnext/projects/PCWE/PCWE.software-package-manifest.json',)
+string = json.load(f)['identification']['name']
+print(string)")
 
-    # rename upperdir and remove old instance on SD card
-    echo "Renaming SD card upperdir"
-    cp -a /media/rfs/externalsd/upperdir/opt/plcnext/projects /media/rfs/externalsd/upperdir/opt/plcnext/projects1
-    #rm -r /media/rfs/externalsd/upperdir/opt/plcnext/projects
+SD_project_name=$(python3 -c "import sys, json;
+f = open('/media/rfs/externalsd/upperdir/opt/plcnext/projects/PCWE/PCWE.software-package-manifest.json',)
+string = json.load(f)['identification']['name']
+print(string)")
 
-    # copy project from PLC to SD and remove old instance from PLC
-    echo "Moving from PLC to SD"
-    #cp -a /media/rfs/internalsd/upperdir/opt/plcnext/projects /media/rfs/externalsd/upperdir/opt/plcnext/
-    mkdir -p /media/rfs/externalsd/upperdir/opt/plcnext/project_archive
-    cp -a /media/rfs/internalsd/upperdir/opt/plcnext/projects /media/rfs/externalsd/upperdir/opt/plcnext/project_archive/$archive_file_name
-    rm -r /media/rfs/internalsd/upperdir/opt/plcnext/projects
+function fileTransfer() {
+sudo /usr/sbin/sdcard_state.sh request_deactivation
+echo "SD card deactivated"
 
-    # copy project from SD to PLC and remove old instance from SD
-    echo "Moving from SD to PLC"
-    cp -a /media/rfs/externalsd/upperdir/opt/plcnext/projects1 /media/rfs/internalsd/upperdir/opt/plcnext/
-    rm -r /media/rfs/externalsd/upperdir/opt/plcnext/projects1
+echo "Project on PLC: $PLC_project_name"
+echo "Project on SD card: $SD_project_name"
 
-    # rename upperdir1 on PLC back to upperdir and remove upperdir1
-    echo "Renaming PLC upperdir"
-    cp -a /media/rfs/internalsd/upperdir/opt/plcnext/projects1 /media/rfs/internalsd/upperdir/opt/plcnext/projects
-    rm -r /media/rfs/internalsd/upperdir/opt/plcnext/projects1
+# rename projects and remove old instance on SD card
+echo "Renaming SD card projects folders"
+cp -a /media/rfs/externalsd/upperdir/opt/plcnext/projects /media/rfs/externalsd/upperdir/opt/plcnext/projects1
+rm -r /media/rfs/externalsd/upperdir/opt/plcnext/projects
 
-    echo "PLC and SD swapped"
-    sudo reboot
-  }
+# copy project from PLC to SD and remove old instance from PLC
+echo "Moving $PLC_project_name from PLC to SD"
+echo "Archiving $PLC_project_name as $archive_file_name"
+cp -a /media/rfs/internalsd/upperdir/opt/plcnext/projects /media/rfs/externalsd/upperdir/opt/plcnext/
+mkdir -p /media/rfs/externalsd/upperdir/opt/plcnext/project_archive
+cp -a /media/rfs/internalsd/upperdir/opt/plcnext/projects /media/rfs/externalsd/upperdir/opt/plcnext/project_archive/$archive_file_name
+rm -r /media/rfs/internalsd/upperdir/opt/plcnext/projects
 
-  sleep 45
+# copy project from SD to PLC and remove old instance from SD
+echo "Moving $SD_project_name from SD to PLC"
+cp -a /media/rfs/externalsd/upperdir/opt/plcnext/projects1 /media/rfs/internalsd/upperdir/opt/plcnext/
+rm -r /media/rfs/externalsd/upperdir/opt/plcnext/projects1
 
-  if [ -f "$FLAG_FILE" ]; then
-    echo "Reboot detected, not performing swap"
-    exit 0
-  fi
+# rename projects1 on PLC back to projects and remove projects1
+echo "Renaming PLC projects folders"
+cp -a /media/rfs/internalsd/upperdir/opt/plcnext/projects1 /media/rfs/internalsd/upperdir/opt/plcnext/projects
+rm -r /media/rfs/internalsd/upperdir/opt/plcnext/projects1
 
-  if [ -d /media/rfs/externalsd/upperdir/opt/plcnext/projects ]; then
-    echo "SD card inserted, performing swap"
-    fileTransfer
-    exit 0
-  else
-    echo "No SD card inserted"
-    exit 0
-  fi
+new_PLC_project_name=$(python3 -c "import sys, json;
+f = open('/media/rfs/internalsd/upperdir/opt/plcnext/projects/PCWE/PCWE.software-package-manifest.json',)
+string = json.load(f)['identification']['name']
+print(string)")
+
+new_SD_project_name=$(python3 -c "import sys, json;
+f = open('/media/rfs/externalsd/upperdir/opt/plcnext/projects/PCWE/PCWE.software-package-manifest.json',)
+string = json.load(f)['identification']['name']
+print(string)")
+
+echo "Project on PLC: $new_PLC_project_name"
+echo "Project on SD card: $new_SD_project_name"
+echo "Rebooting ..."
+sudo reboot
+}
+# use systemd systemctl or entr to create a rule to run a script to organize files correctly when a PCWE folder is uploaded
+# and then runs swap
+
+sleep 45
+
+if [ -f "$FLAG_FILE" ]; then
+  echo "Reboot detected, not performing swap"
+  exit 0
+fi
+
+if [ -d /media/rfs/externalsd/upperdir/opt/plcnext/projects ]; then
+  echo "SD card inserted, performing swap"
+  sudo /etc/init.d/plcnext stop
+  fileTransfer
+  exit 0
+else
+  echo "No SD card inserted"
+  exit 0
+fi
 } & disown
 
 
