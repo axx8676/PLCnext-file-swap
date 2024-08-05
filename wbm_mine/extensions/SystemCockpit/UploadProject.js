09a100024d50ed7c2035953f17afa0cc3026b5f7
axx8676 <axel.s.thompson-1@ou.edu>
2024-08-05 16:48:14 -0500

    project now uploads in chunks

--- wbm_mine/extensions/SystemCockpit/UploadProject.js
+++ wbm_mine/extensions/SystemCockpit/UploadProject.js
@@ -1,34 +1,238 @@
-function handleFileInputClick(inputId) {
+fileChunks = [];
+BytesPerChunk = 1024 * 1024;
+fileSize = 0;
+fileLoadedSize = 0;
+currChunkNumber = 0;
+currChunkBlobIdx = 0;
+prevChunkLoadedSize = 0;
+chunksCompletedTransfers = 0;
+
+function CleanUpPageData()
+{
+    delete BytesPerChunk;
+    delete fileSize;
+    delete fileLoadedSize;
+    delete currChunkNumber;
+    delete currChunkBlobIdx;
+    delete prevChunkLoadedSize;
+    delete chunksCompletedTransfers;
+}
+
+function handleFileInputClick(inputId) 
+{
     document.getElementById(inputId).click();
 }
 
 // prompts the user for a file
-function fileSelected() {
-    const input = document.getElementById('id_project_file_input');
-    const file = input.files[0];
-    if (file) {
-        console.log(`File selected: ${file.name}`);
+function fileSelected() 
+{
+    var input = document.getElementById('id_project_file_input');
+    var file = input.files[0];
 
-        // create formData object to hold file data
-        const formData = new FormData();
-        formData.append('project_file', file);
-
-        // create XMLHttpRequest to send the file
-        const xhr = new XMLHttpRequest();
-        xhr.open('POST', '/upload', true);
-
-        xhr.onload = function () {
-            if (xhr.status === 200) {
-                console.log('File uploaded successfully');
-            }
-            else {
-                console.error('File upload failed');
-            }
-        };
-
-        xhr.send(formData);
+    var formatFilter = /(\.zip)$/i;
+    if (! formatFilter.test(file.name)) {
+        console.log("Invalid format of PLCnext Project. Only .zip file format is accepted")
+        return;
     }
-    else {
-        console.log("No file selected");
+    console.log(`File selected: ${file.name}`);
+
+    fileSize = file.size;
+
+    console.log('PLCnext app container file size = ' + fileSize);
+
+    ReadFileSlices(file);
+}
+
+function ReadFileSlices(file)
+{
+    var startUpload = 0;
+    var endUpload = BytesPerChunk;
+
+    fileChunks = [];
+
+    var slice = file.slice(startUpload, endUpload);
+
+    var fileReader = new FileReader();
+    fileReader.onload = function(e){
+        fileChunks.push(e.target.result);
+        startUpload = endUpload;
+        endUpload = startUpload + BytesPerChunk;
+
+        if (startUpload < fileSize)
+        {
+            slice = file.slice(startUpload, endUpload);
+            fileReader.readAsArrayBuffer(slice);
+        }
+        else
+        {
+            resultFileSize = WbmUtilities.BytesToSize(file.size);
+            StartUploading();
+        }
+    }
+    if(startUpload < fileSize)
+    {
+        fileReader.readAsArrayBuffer(slice);
     }
 }
+
+function StartUploading() 
+{
+    console.log('Starting project installation');
+    console.log('slices size = ' + fileChunks.length);
+    
+    fileLoadedSize = 0;
+    chunksCompletedTransfers = 0;
+    uploadIsSuccessful = false;
+    currChunkBlobIdx = 0;
+    currChunkNumber = 0;
+
+    abortUploading = false;
+
+    let response = ProjectService.StartDownloading(fileSize);
+    if (response.error != true)
+    {
+        UploadNextFileChunk();
+    }
+}
+
+function UploadNextFileChunk()
+{
+    if ((currChunkNumber >= fileChunks.length) || (currChunkBlobIdx >= fileChunks.length))
+    {
+        return;
+    }
+
+    currChunkNumber = currChunkBlobIdx + 1;
+    prevChunkLoadedSize = 0;
+    ProjectService.UploadProject(fileChunks[currChunkBlobIdx]);
+}
+
+//TODO: OnUploadProgress
+function OnUploadProgress(evt)
+{
+    if(evt.lengthComputable == true)
+    {
+        fileLoadedSize += (evt.loaded - prevChunkLoadedSize);
+        prevChunkLoadedSize = evt.loaded;
+
+        var uploadPercentCompleted = Math.round(fileLoadedSize/fileSize * 100);
+        var dataBytesRemaining = WbmUtilities.BytesToSize((fileSize - fileLoadedSize));
+
+        if (uploadPercentCompleted == 100) {
+            progressPercent = 0;
+        }
+    }
+}
+
+function OnUploadComplete()
+{
+    progressPercent = 100;
+
+    if(uploadIsSuccessful == true && abortUploading == false)
+    {
+        console.log("Upload complete");
+        let response = ProjectService.FinishDownloading();
+        if(response.error == true)
+        {
+            console.log('function finishDownloading failed');
+        }
+    }
+}
+
+function AbortUploading() {
+    abortUploading = true;
+}
+
+ProjectService = (function () {
+    let Public = {
+        StartDownloading: function (fileSize)
+        {
+            let data = "fileSize=" + encodeURIComponent(fileSize);
+            let ajaxReqConfig = AjaxInterface.CreateSyncRequestConfig(
+                "upload/Project/StartDownloading?" + data,
+                AjaxInterface.AjaxRequestType.GET,
+                AjaxInterface.AjaxRequestDataType.JSON,
+                null,
+                null);
+            return AjaxInterface.PerformSyncRequest(ajaxReqConfig);
+        },
+
+        UploadProject: function (uploadData) 
+        {
+            var xhrGlobal = $.ajax({
+                url: "upload/Project/UploadProject",
+                type: "POST",
+                data: uploadData,
+                processData: false,
+                contentType: "application",
+                dataType: "zip",
+                async: true,
+                cache: false,
+                timeout: 3000,
+                xhr: function () {
+                    var xhr = $.ajaxSettings.xhr();
+                    xhr.upload.onprogress = function (evt) {
+                        if (abortUploading == true) {
+                            xhrGlobal.abort();
+                        }
+                        OnUploadProgress(evt);
+                    };
+                    xhr.upload.onload = function () {
+
+                    };
+                    return xhr;
+                },
+                success: function (response) {
+                    if (response.error) {
+                        uploadIsSuccessful = false;
+                        AbortUploading();
+                        OnUploadComplete();
+                        console.log(response.errorText);
+                    }
+                    else {
+                        if (currChunkNumber >= fileChunks.length) {
+                            console.log("Upload of update container was successful");
+                            uploadIsSuccessful = true;
+                        }
+                        else {
+                            console.log('Upload of update container chunk ' + currChunkNumber + ' successful');
+                            currChunkBlobIdx++;
+                            if (abortUploading == false) {
+                                UploadNextFileChunk();
+                            }
+                        }
+                    }
+                },
+                error: function (req, status, err) {
+                    console.log('General error occurred: ', status, err);
+                    uploadIsSuccessful = false;
+                    if(abortUploading == false) {
+                        console.log('Aborting');
+                        xhrGlobal.abort();
+                    }
+                },
+                complete: function () {
+                    chunksCompletedTransfers++;
+                    if (currChunkNumber == fileChunks.length && chunksCompletedTransfers == fileChunks.length) {
+                        uploadIsSuccessful = true;
+                        console.log('Upload Completed');
+                        OnUploadComplete();
+                    }
+                },
+            });
+        },
+
+        FinishDownloading: function ()
+        {
+            let ajaxReqConfig = AjaxInterface.CreateSyncRequestConfig(
+                "upload/Project/FinishDownloading",
+                AjaxInterface.AjaxRequestType.POST,
+                AjazInterface.AjaxRequestDataType.JSON,
+                "",
+                null);
+            return AjaxInterface.PerformSyncRequest(ajaxReqConfig);
+        }
+    }
+
+    return Public;
+})();
